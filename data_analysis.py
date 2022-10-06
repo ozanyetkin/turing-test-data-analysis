7b74a524671a4de6bb6cf576a77a048776ae2538
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-10-06 13:54:07 +0300

    data red successfully for three datasets

---
+++ data_analysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+cardio_alco = pd.read_csv("./data/cardio_alco.csv")
+cardio_base = pd.read_csv("./data/cardio_base.csv")
+covid_data = pd.read_csv("./data/covid_data.csv")
+
+print(cardio_alco)
+print(cardio_base)
+print(covid_data)
